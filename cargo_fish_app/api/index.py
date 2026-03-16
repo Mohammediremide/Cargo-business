53f4911fd651776b6f0d109a34cb4d20234eea6a
CodexSandboxOffline <odewunmimohammed@gmail.com>
2026-03-16 18:12:47 +0100

    Revert "Add Vercel entrypoint for subdir deploys"
    This reverts commit 6ec17a7dc0f278086aa8828d74fc4ab512f0192d.

--- cargo_fish_app/api/index.py
+++
@@ -1,5 +0,0 @@
-"""Vercel entrypoint for both repo-root and subdir deployments."""
-try:
-    from cargo_fish_app.app import app  # Repo root deployment
-except Exception:
-    from app import app  # Subdirectory deployment
